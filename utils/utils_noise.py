32c031a226873c0d53a8899c184925693818c6eb
liux520 <96382721+liux520@users.noreply.github.com>
2024-09-04 14:01:37 +0800

    Update utils_noise.py

--- utils/utils_noise.py
+++ utils/utils_noise.py
@@ -4,6 +4,7 @@ import numpy as np
 from PIL import Image
 import cv2
 import torchvision.transforms
+from torchvision.transforms.functional import rgb_to_grayscale
 
 """
 1. Gaussian noise
@@ -30,6 +31,7 @@ random_generate_gaussian_noise_pt
 random_add_gaussian_noise_pt
 '''
 
+
 # -------------------------------------------------------------------- #
 # --------------------random_add_gaussian_noise----------------------- #
 # -------------------------------------------------------------------- #
@@ -45,12 +47,14 @@ def random_add_gaussian_noise_pt(img, sigma_range=(0, 1.0), gray_prob=0, clip=Tr
         out = (out * 255.0).round() / 255.
     return out
 
+
 def random_generate_gaussian_noise_pt(img, sigma_range=(0, 10), gray_prob=0):
     sigma = torch.rand(
         img.size(0), dtype=img.dtype, device=img.device) * (sigma_range[1] - sigma_range[0]) + sigma_range[0]
     gray_noise = torch.rand(img.size(0), dtype=img.dtype, device=img.device)
     gray_noise = (gray_noise < gray_prob).float()
     return generate_gaussian_noise_pt(img, sigma, gray_noise)
+
 
 def generate_gaussian_noise_pt(img, sigma=10, gray_noise=0):
     """Add Gaussian noise (PyTorch version).
@@ -80,6 +84,7 @@ def generate_gaussian_noise_pt(img, sigma=10, gray_noise=0):
     if cal_gray_noise:
         noise = noise * (1 - gray_noise) + noise_gray * gray_noise
     return noise
+
 
 def add_gaussian_noise_pt(img, sigma=10, gray_noise=0, clip=True, rounds=False):
     """Add Gaussian noise (PyTorch version).
@@ -116,12 +121,14 @@ def random_add_poisson_noise_pt(img, scale_range=(0, 1.0), gray_prob=0, clip=Tru
         out = (out * 255.0).round() / 255.
     return out
 
+
 def random_generate_poisson_noise_pt(img, scale_range=(0, 1.0), gray_prob=0):
     scale = torch.rand(
         img.size(0), dtype=img.dtype, device=img.device) * (scale_range[1] - scale_range[0]) + scale_range[0]
     gray_noise = torch.rand(img.size(0), dtype=img.dtype, device=img.device)
     gray_noise = (gray_noise < gray_prob).float()
     return generate_poisson_noise_pt(img, scale, gray_noise)
+
 
 def generate_poisson_noise_pt(img, scale=1.0, gray_noise=0):
     """Generate a batch of poisson noise (PyTorch version)
@@ -142,14 +149,14 @@ def generate_poisson_noise_pt(img, scale=1.0, gray_noise=0):
         gray_noise = gray_noise.view(b, 1, 1, 1)
         cal_gray_noise = torch.sum(gray_noise) > 0
     if cal_gray_noise:
-        #img_gray = rgb_to_grayscale(img, num_output_channels=1)
-        img_gray=img[:,0,:,:]   #size: BHW
-        img_gray=torch.unsqueeze(img_gray,1)
+        img_gray = rgb_to_grayscale(img, num_output_channels=1)
+        # img_gray=img[:,0,:,:]   #size: BHW
+        img_gray = torch.unsqueeze(img_gray, 1)
         # round and clip image for counting vals correctly
         img_gray = torch.clamp((img_gray * 255.0).round(), 0, 255) / 255.
         # use for-loop to get the unique values for each sample
         vals_list = [len(torch.unique(img_gray[i, :, :, :])) for i in range(b)]
-        vals_list = [2**np.ceil(np.log2(vals)) for vals in vals_list]
+        vals_list = [2 ** np.ceil(np.log2(vals)) for vals in vals_list]
         vals = img_gray.new_tensor(vals_list).view(b, 1, 1, 1)
         out = torch.poisson(img_gray * vals) / vals
         noise_gray = out - img_gray
@@ -160,7 +167,7 @@ def generate_poisson_noise_pt(img, scale=1.0, gray_noise=0):
     img = torch.clamp((img * 255.0).round(), 0, 255) / 255.
     # use for-loop to get the unique values for each sample
     vals_list = [len(torch.unique(img[i, :, :, :])) for i in range(b)]
-    vals_list = [2**np.ceil(np.log2(vals)) for vals in vals_list]
+    vals_list = [2 ** np.ceil(np.log2(vals)) for vals in vals_list]
     vals = img.new_tensor(vals_list).view(b, 1, 1, 1)
     out = torch.poisson(img * vals) / vals
     noise = out - img
@@ -169,6 +176,7 @@ def generate_poisson_noise_pt(img, scale=1.0, gray_noise=0):
     if not isinstance(scale, (float, int)):
         scale = scale.view(b, 1, 1, 1)
     return noise * scale
+
 
 def add_poisson_noise_pt(img, scale=1.0, clip=True, rounds=False, gray_noise=0):
     """Add poisson noise to a batch of images (PyTorch version).
@@ -192,6 +200,7 @@ def add_poisson_noise_pt(img, scale=1.0, clip=True, rounds=False, gray_noise=0):
         out = (out * 255.0).round() / 255.
     return out
 
+
 if __name__ == '__main__':
     gaussian_noise_prob2 = 0.5
     noise_range2 = [1, 25]
@@ -206,12 +215,12 @@ if __name__ == '__main__':
     #
     # img_noise=np.uint8((img_noise.clip(0,1)*255.).round())
 
-    img=Image.open('../dog.jpg')
-    img=torchvision.transforms.ToTensor()(img)
-    img=img.unsqueeze(0)
+    img = Image.open('../dog.jpg')
+    img = torchvision.transforms.ToTensor()(img)
+    img = img.unsqueeze(0)
 
-    img_noise=random_add_poisson_noise_pt(img,poisson_scale_range2,gray_noise_prob2)
+    img_noise = random_add_poisson_noise_pt(img, poisson_scale_range2, gray_noise_prob2)
 
-    img_noise=img_noise.squeeze(0).permute(1,2,0).detach().numpy()
+    img_noise = img_noise.squeeze(0).permute(1, 2, 0).detach().numpy()
     plt.imshow(img_noise)
     plt.show()
